33739c6f5a3d2f63031f3332c4318a5185984fae
Eduardo Biscaia de Queiroz <ebiscaia@me.com>
2022-08-04 13:37:58 +1000

    Added autostart programs

--- .config/qtile/autostart.sh
+++ .config/qtile/autostart.sh
@@ -7,4 +7,5 @@ brave-browser &
 notify-send -a "Ubuntu" "Notification" "Welcome to Qtile"
 feh --bg-fill --randomize ~/Pictures/*
 picom &
-
+/usr/bin/xautolock -time 10 -locker "i3lock-fancy -f 'Binchotan_Sharp-Regular-Nerd-Font-Plus-Font-Awesome-Plus-Font-Logos-(Font-Linux)-Plus-Material-Design-Icons'" -detectsleep &
+copyq &
