026d5f96009e8874a6309c7c5920a49db63e1027
Eduardo Biscaia de Queiroz <ebiscaia@me.com>
2022-08-07 10:45:15 +1000

    Deleted file test.py

--- .config/nvim/lua/eddie/test.py
+++
@@ -1,8 +0,0 @@
-from tkinter import *
-
-class MyGui:
-    def __init__()self:
-        self.main_window = Tk()
-    #Display a title
-self.main_window.title(Hello World)
-self.label1 = Label(self.main_window)
