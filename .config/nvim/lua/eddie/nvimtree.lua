5b2836efa6363b4129b6f75db9b2000526e3d419
Eduardo Biscaia de Queiroz <ebiscaia@me.com>
2022-08-07 10:58:50 +1000

    Reformatted file

--- .config/nvim/lua/eddie/nvimtree.lua
+++ .config/nvim/lua/eddie/nvimtree.lua
@@ -1,62 +1,63 @@
-require("nvim-tree").setup{
-disable_netrw = true,
-  hijack_netrw =true,
+require("nvim-tree").setup {
+  disable_netrw = true,
+  hijack_netrw = true,
 
-renderer = {
-  icons = {
-          webdev_colors = true,
-          git_placement = "before",
-          padding = " ",
-          symlink_arrow = " ➛ ",
-          show = {
-            file = true,
-            folder = true,
-            folder_arrow = true,
-            git = true,
-          },
-          glyphs = {
-            default = "",
-            symlink = "",
-            bookmark = "",
-            folder = {
-              arrow_closed = "",
-              arrow_open = "",
-              default = "",
-              open = "",
-              empty = "",
-              empty_open = "",
-              symlink = "",
-              symlink_open = "",
-            },
-            git = {
-              unstaged = "✗",
-              staged = "✓",
-              unmerged = "",
-              renamed = "➜",
-              untracked = "★",
-              deleted = "",
-              ignored = "◌",
-            },
-  },},
-        indent_markers = {
-          enable = true,
-          icons = {
-            corner = "└",
-            edge = "│",
-            item = "│",
-            none = " ",
-          },},
- },
-diagnostics = {
-      enable = true,
-      show_on_dirs = true,
-      icons = {
-        error = "",
-		    warning = "",
-		    hint = "",
-		    info = "",
+  renderer = {
+    icons = {
+      webdev_colors = true,
+      git_placement = "before",
+      padding = " ",
+      symlink_arrow = " ➛ ",
+      show = {
+        file = true,
+        folder = true,
+        folder_arrow = true,
+        git = true,
+      },
+      glyphs = {
+        default = "",
+        symlink = "",
+        bookmark = "",
+        folder = {
+          arrow_closed = "",
+          arrow_open = "",
+          default = "",
+          open = "",
+          empty = "",
+          empty_open = "",
+          symlink = "",
+          symlink_open = "",
+        },
+        git = {
+          unstaged = "ﱤ",
+          staged = "",
+          commited = "+",
+          unmerged = "",
+          renamed = "➜",
+          untracked = "★",
+          deleted = "",
+          ignored = "◌",
+        },
       },
     },
+    indent_markers = {
+      enable = true,
+      icons = {
+        corner = "└",
+        edge = "│",
+        item = "│",
+        none = " ",
+      },
+    },
+  },
+  diagnostics = {
+    enable = true,
+    show_on_dirs = true,
+    icons = {
+      error = "",
+      warning = "",
+      hint = "",
+      info = "",
+    },
+  },
 }
-
-
