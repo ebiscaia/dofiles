517ae273fbfed1f7f57f9af95ef73a7931ab06f2
Eduardo Biscaia de Queiroz <ebiscaia@me.com>
2022-08-07 12:03:13 +1000

    Removed garbage from file

--- .config/nvim/lua/eddie/autocommands.lua
+++ .config/nvim/lua/eddie/autocommands.lua
@@ -3,4 +3,3 @@ local atcmd = vim.api.nvim_create_autocmd
 atcmd("FileType", { pattern = { "python" }, command = [[nmap <F5> <Esc>:w<CR>:TermExec cmd="python3 %"<CR>]] })
 atcmd("FileType", { pattern = { "python" }, command = [[imap <F5> <Esc>:w<CR>:TermExec cmd="python3 %"<CR>]] })
 atcmd("BufWritePre", { pattern = { "*.py" }, callback = function () vim.lsp.buf.format() end })
--- )
