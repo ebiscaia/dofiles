6241d7829e33512594876b8afbea9c5b9af2fc1b
Eduardo Biscaia de Queiroz <ebiscaia@me.com>
2022-08-08 20:04:10 +1000

    # On branch main # Your branch is up to date with 'origin/main'. # # Changes to be committed: #   (use "git restore --staged <file>..." to unstage) # 	modified:   .config/nvim/lua/eddie/plugins.lua

--- .config/nvim/lua/eddie/plugins.lua
+++ .config/nvim/lua/eddie/plugins.lua
@@ -94,7 +94,19 @@ use "akinsho/toggleterm.nvim"
 use 'TimUntersberger/neogit'
 use 'sindrets/diffview.nvim'
 use 'lewis6991/gitsigns.nvim'
---
+use {'akinsho/git-conflict.nvim', config = function()
+    require('git-conflict').setup(
+      {
+  default_mappings = true, -- disable buffer local mapping created by this plugin
+  disable_diagnostics = false, -- This will disable the diagnostics in a buffer whilst it is conflicted
+  highlights = { -- They must have background color, otherwise the default color will be used
+    incoming = 'DiffText',
+    current = 'DiffAdd',
+  }
+}
+    )
+end}
+
 -- Null-LS
 use "jose-elias-alvarez/null-ls.nvim"
 
