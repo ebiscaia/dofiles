90fd72cef911d42dadc1cc2dea016341e1adcd5f
Eduardo Biscaia de Queiroz <ebiscaia@me.com>
2022-08-10 19:46:47 +1000

    First commit

---
+++ .config/qtile/cScheme.py
@@ -0,0 +1,83 @@
+# This file is to provide a quick way to swap color in qtile
+# Enter the colorscheme by following the scheme bellow:
+#
+# colorscheme= [bg, fb, red1, red2, green1, green2,...]
+# every color must be repeated twice because qtile allows gradients
+
+gruvboxDark = [
+    ["#282828", "#282828"],
+    ["#ebdbb2", "#ebdbb2"],
+    ["#cc241d", "#cc241d"],
+    ["#fb4934", "#fb4934"],
+    ["#98971a", "#98971a"],
+    ["#b8bb26", "#b8bb26"],
+    ["#d79921", "#d79921"],
+    ["#fabd2f", "#fabd2f"],
+    ["#458588", "#458588"],
+    ["#83a598", "#83a598"],
+    ["#b16286", "#b16286"],
+    ["#d3869b", "#d3869b"],
+    ["#689d6a", "#689d6a"],
+    ["#8ec07c", "#8ec07c"],
+    ["#928374", "#928374"],
+    ["#fbf0c9", "#fbf0c9"],
+]
+
+
+gruvboxLight = [
+    ["#fbf1c7", "#fbf1c7"],
+    ["#3c3836", "#3c3836"],
+    ["#cc241d", "#cc241d"],
+    ["#9d0006", "#9d0006"],
+    ["#98971a", "#98971a"],
+    ["#79740e", "#79740e"],
+    ["#d79921", "#d79921"],
+    ["#b57614", "#b57614"],
+    ["#458588", "#458588"],
+    ["#076678", "#076678"],
+    ["#b16286", "#b16286"],
+    ["#8f3f71", "#8f3f71"],
+    ["#689d6a", "#689d6a"],
+    ["#427b58", "#427b58"],
+    ["#928374", "#928374"],
+    ["#7c6f64", "#7c6f64"],
+]
+
+
+nord = [
+    ["#2e3340", "#2e3340"],
+    ["#d8dde9", "#d8dde9"],
+    ["#bf616a", "#bf616a"],
+    ["#bf616a", "#bf616a"],
+    ["#88c0d0", "#88c0d0"],
+    ["#8fbcbb", "#8fbcbb"],
+    ["#ebcb8b", "#ebcb8b"],
+    ["#ebcb8b", "#ebcb8b"],
+    ["#81a1c1", "#81a1c1"],
+    ["#5e81ac", "#5e81ac"],
+    ["#b48ead", "#b48ead"],
+    ["#b48ead", "#b48ead"],
+    ["#a3be8c", "#a3be8c"],
+    ["#a3be8c", "#a3be8c"],
+    ["#3b4252", "#3b4252"],
+    ["#4c566a", "#4c566a"],
+]
+
+dracula = [
+    ["#282a36", "#282a36"],
+    ["#f2f2f8", "#f2f2f8"],
+    ["#ff5555", "#ff5555"],
+    ["#ff6e67", "#ff6e67"],
+    ["#8be9fd", "#8be9fd"],
+    ["#9aedfe", "#9aedfe"],
+    ["#f1fa8c", "#f1fa8c"],
+    ["#f4f99d", "#f4f99d"],
+    ["#bd93f9", "#bd93f9"],
+    ["#caa9fa", "#caa9fa"],
+    ["#ff79c6", "#ff79c6"],
+    ["#ff92d0", "#ff92d0"],
+    ["#50fa7b", "#50fa7b"],
+    ["#5af78e", "#5af78e"],
+    ["#afafbf", "#afafbf"],
+    ["#e5e5d1", "#e5e5d1"],
+]
