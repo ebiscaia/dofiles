f73986791c45c3df00455295129d2068d2369baf
Eduardo Biscaia de Queiroz <ebiscaia@me.com>
2022-08-10 19:53:49 +1000

    Create variable to capture Distribution name

--- .config/qtile/autostart.sh
+++ .config/qtile/autostart.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+#Discover the dist name
+DIST=$(lsb_release -a | cut -f2 | sed -n 2p)
+
 numlockx on
 dunst &
 alacritty &
 brave-browser &
-notify-send -a "Ubuntu" "Notification" "Welcome to Qtile"
+notify-send -a $DIST "Notification" "Welcome to Qtile"
 feh --bg-fill --randomize ~/Pictures/*
 picom &
 /usr/bin/xautolock -time 10 -locker "i3lock-fancy -f 'Binchotan_Sharp-Regular-Nerd-Font-Plus-Font-Awesome-Plus-Font-Logos-(Font-Linux)-Plus-Material-Design-Icons'" -detectsleep &
