5061878be40ff46f6405081654febf32c6f6fabd
Eduardo Biscaia de Queiroz <ebiscaia@me.com>
2022-08-11 15:34:01 +1000

    Added "*.lua" to be autoformatted

--- .config/nvim/lua/eddie/autocommands.lua
+++ .config/nvim/lua/eddie/autocommands.lua
@@ -5,3 +5,5 @@ atcmd("FileType", { pattern = { "python" }, command = [[imap <F5> <Esc>:w<CR>:Te
 atcmd("BufWritePre", { pattern = { "*.py" }, callback = function () vim.lsp.buf.formatting_sync() end })
 atcmd("FileType", { pattern = { "python" }, command = [[nmap <F5> <Esc>:w<CR>:TermExec cmd="<ESC>;python3 %"<CR>]] })
 atcmd("FileType", { pattern = { "python" }, command = [[imap <F5> <Esc>:w<CR>:TermExec cmd="<ESC>;python3 %"<CR>]] })
+-- Formatting Files
+atcmd("BufWritePre", { pattern = { "*.py", "*.lua" }, callback = function () vim.lsp.buf.formatting_sync() end })
