caf324203564fe03c71295dcec59cf6b06969350
Eduardo Biscaia de Queiroz <ebiscaia@me.com>
2022-08-11 15:43:12 +1000

    Staged to match file

--- .config/nvim/lua/eddie/autocommands.lua
+++ .config/nvim/lua/eddie/autocommands.lua
@@ -1,9 +1,12 @@
 local atcmd = vim.api.nvim_create_autocmd
 
-atcmd("FileType", { pattern = { "python" }, command = [[nmap <F5> <Esc>:w<CR>:TermExec cmd="python3 %"<CR>]] })
-atcmd("FileType", { pattern = { "python" }, command = [[imap <F5> <Esc>:w<CR>:TermExec cmd="python3 %"<CR>]] })
-atcmd("BufWritePre", { pattern = { "*.py" }, callback = function () vim.lsp.buf.formatting_sync() end })
 atcmd("FileType", { pattern = { "python" }, command = [[nmap <F5> <Esc>:w<CR>:TermExec cmd="<ESC>;python3 %"<CR>]] })
 atcmd("FileType", { pattern = { "python" }, command = [[imap <F5> <Esc>:w<CR>:TermExec cmd="<ESC>;python3 %"<CR>]] })
+
+
+
+
+
+
 -- Formatting Files
 atcmd("BufWritePre", { pattern = { "*.py", "*.lua" }, callback = function () vim.lsp.buf.formatting_sync() end })
