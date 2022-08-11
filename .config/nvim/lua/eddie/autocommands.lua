1b6796f46671263bfef25c5118ebd9ed93f07e4b
Eduardo Biscaia de Queiroz <ebiscaia@me.com>
2022-08-11 17:39:20 +1000

    Added clear to allow python3 command to run properly

--- .config/nvim/lua/eddie/autocommands.lua
+++ .config/nvim/lua/eddie/autocommands.lua
@@ -1,11 +1,9 @@
 local atcmd = vim.api.nvim_create_autocmd
 
-atcmd("FileType", { pattern = { "python" }, command = [[nmap <F5> <Esc>:w<CR>:TermExec cmd="<ESC>;python3 %"<CR>]] })
-atcmd("FileType", { pattern = { "python" }, command = [[imap <F5> <Esc>:w<CR>:TermExec cmd="<ESC>;python3 %"<CR>]] })
-
-
-
-
+atcmd("FileType",
+  { pattern = { "python" }, command = [[nmap <F5> <Esc>:w<CR>:TermExec cmd="clear; clear; python3 %"<CR>]] })
+atcmd("FileType",
+  { pattern = { "python" }, command = [[imap <F5> <Esc>:w<CR>:TermExec cmd="clear; clear; python3 %"<CR>]] })
 
 
 -- Formatting Files
