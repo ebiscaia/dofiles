0e421d5833123e757d17157b9185e6192e742bff
Eduardo Biscaia de Queiroz <ebiscaia@me.com>
2022-08-16 16:35:13 +1000

    Replaced <C-F5> to <F4>

--- .config/nvim/lua/eddie/autocommands.lua
+++ .config/nvim/lua/eddie/autocommands.lua
@@ -1,9 +1,9 @@
 local atcmd = vim.api.nvim_create_autocmd
 
 atcmd("FileType",
-  { pattern = { "python" }, command = [[nmap <F5> <Esc>:w<CR>:TermExec cmd="clear; clear; python3 %"<CR>]] })
+  { pattern = { "python" }, command = [[nmap <F4> <Esc>:w<CR>:TermExec cmd="clear; clear; python3 %"<CR>]] })
 atcmd("FileType",
-  { pattern = { "python" }, command = [[imap <F5> <Esc>:w<CR>:TermExec cmd="clear; clear; python3 %"<CR>]] })
+  { pattern = { "python" }, command = [[imap <F4> <Esc>:w<CR>:TermExec cmd="clear; clear; python3 %"<CR>]] })
 
 
 -- Formatting Files
