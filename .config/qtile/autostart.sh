24637a6530c76efee1b821852862e74baf36a6e5
Eduardo Biscaia de Queiroz <ebiscaia@me.com>
2022-08-17 12:41:04 +1000

    removed font from command | don't pick

--- .config/qtile/autostart.sh
+++ .config/qtile/autostart.sh
@@ -7,5 +7,5 @@ brave-browser &
 notify-send -a $DIST "Notification" "Welcome to Qtile"
 feh --bg-fill --randomize ~/Pictures/*
 picom &
-/usr/bin/xautolock -time 10 -locker "i3lock-fancy -f 'Binchotan_Sharp-Regular-Nerd-Font-Plus-Font-Awesome-Plus-Font-Logos-(Font-Linux)-Plus-Material-Design-Icons'" -detectsleep &
+/usr/bin/xautolock -time 10 -locker "i3lock-fancy -f" -detectsleep &
 copyq &
