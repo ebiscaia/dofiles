45891a8315ad0abf75c4bb348f71c358f0dbb77a
Eduardo Biscaia de Queiroz <ebiscaia@me.com>
2022-08-20 20:30:50 +1000

    Redefined

--- .config/nvim/lua/eddie/toggleterm.lua
+++ .config/nvim/lua/eddie/toggleterm.lua
@@ -1,26 +1,39 @@
-require("toggleterm").setup{
--- size can be a number or function which is passed the current terminal
-  size = 20,
+local status_ok, toggleterm = pcall(require, "toggleterm")
+if not status_ok then
+  return
+end
+
+toggleterm.setup({
+  size = 15,
   open_mapping = [[<c-\>]],
-  hide_numbers = true, -- hide the number column in toggleterm buffers
+  hide_numbers = true,
   shade_filetypes = {},
-  shade_terminals = true, -- NOTE: this option takes priority over highlights specified so if you specify Normal highlights you should set this to false
-  shading_factor = '1', -- the degree by which to darken to terminal colour, default: 1 for dark backgrounds, 3 for light
+  shade_terminals = true,
+  shading_factor = 3,
   start_in_insert = true,
-  insert_mappings = true, -- whether or not the open mapping applies in insert mode
-  terminal_mappings = true, -- whether or not the open mapping applies in the opened terminals
+  insert_mappings = true,
   persist_size = true,
-  persist_mode = true, -- if set to true (default) the previous terminal mode will be remembered
-  direction = 'float',
-  close_on_exit = true, -- close the terminal window when the process exits
-  shell = 'zsh' , -- change the default shell
-  -- This field is only relevant if direction is set to 'float'
+  direction = "horizontal",
+  close_on_exit = true,
+  shell = vim.o.shell,
   float_opts = {
-    -- The border key is *almost* the same as 'nvim_open_win'
-    -- see :h nvim_open_win for details on borders however
-    -- the 'curved' border is a custom border type
-    -- not natively supported but implemented in this plugin.
-    border = 'curved',
-    winblend = 3,
+    border = "curved",
+    winblend = 0,
+    highlights = {
+      border = "Normal",
+      background = "Normal",
+    },
   },
-}
+})
+
+function _G.set_terminal_keymaps()
+  local opts = { noremap = true }
+  vim.api.nvim_buf_set_keymap(0, 't', '<esc>', [[<C-\><C-n>]], opts)
+  vim.api.nvim_buf_set_keymap(0, 't', 'jj', [[<C-\><C-n>]], opts)
+  vim.api.nvim_buf_set_keymap(0, 't', '<C-h>', [[<C-\><C-n><C-W>h]], opts)
+  vim.api.nvim_buf_set_keymap(0, 't', '<C-j>', [[<C-\><C-n><C-W>j]], opts)
+  vim.api.nvim_buf_set_keymap(0, 't', '<C-k>', [[<C-\><C-n><C-W>k]], opts)
+  vim.api.nvim_buf_set_keymap(0, 't', '<C-l>', [[<C-\><C-n><C-W>l]], opts)
+end
+
+vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
