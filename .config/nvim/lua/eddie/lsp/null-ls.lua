86b014031d387360847a473bf8196e8431439569
Eduardo Biscaia de Queiroz <ebiscaia@me.com>
2022-08-21 14:04:15 +1000

    Added "--tab-width", "2" to prettier

--- .config/nvim/lua/eddie/lsp/null-ls.lua
+++ .config/nvim/lua/eddie/lsp/null-ls.lua
@@ -11,7 +11,10 @@ local formatting = null_ls.builtins.formatting
 null_ls.setup {
   debug = false,
   sources = {
-    formatting.prettier.with { extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" } },
+    formatting.prettier.with { extra_args = { "--tab-width", "2",
+      "--no-semi",
+      "--single-quote",
+      "--jsx-single-quote" } },
     formatting.black.with { extra_args = { "--fast" } },
     --        -- formatting.yapf,
     --    formatting.stylua
