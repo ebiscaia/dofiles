a9d20dbe1d99eb65510c3beea072625528f2bd2c
Eduardo Biscaia de Queiroz <ebiscaia@me.com>
2022-08-22 17:58:12 +1000

    Reordered tasks

--- .config/qtile/autostart.sh
+++ .config/qtile/autostart.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-dunst &
 brave-browser &
-$GUESSED_TERMINAL &
+dunst &
 notify-send -a $DIST "Notification" "Welcome to Qtile"
 feh --bg-fill --randomize ~/Pictures/*
 picom &
 /usr/bin/xautolock -time 10 -locker "i3lock-fancy -f" -detectsleep &
 copyq &
+$GUESSED_TERMINAL &
 numlockx on
